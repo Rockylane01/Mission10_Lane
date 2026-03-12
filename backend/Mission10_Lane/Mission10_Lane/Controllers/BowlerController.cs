9ac74d5f05edd788e64df2d4ccd45f99cad4bb83
Rockylane01 <rockylane01@gmail.com>
2026-03-11 23:49:23 -0600

    Ignore Visual Studio .vs folder

---
+++ backend/Mission10_Lane/Mission10_Lane/Controllers/BowlerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mission10_Lane.Models;
+
+namespace Mission10_Lane.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BowlerController : ControllerBase
+    {
+        private BowlingLeagueContext _context;
+
+        public BowlerController(BowlingLeagueContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet(Name = "GetBowlingLeague")]
+        public IEnumerable<Bowler> Get()
+        {
+            var bowlers = _context.Bowlers.ToList();
+
+            return bowlers;
+        }
+    }
+}
